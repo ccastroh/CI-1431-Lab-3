4f7b32977e7ec610cccd8e1a5976770d168439b9
Carlos A Castro <carlosacastroh@gmail.com>
2014-08-21 12:41:06 -0600

    Versión Inicial

---
+++ AsciiArt.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Clase con dibujitos en ASCII
+ * 
+ * @author Carlos A Castro
+ */
+public class AsciiArt {
+    public AsciiArt() {
+    }
+    
+    public void imprimir(String archivo) throws IOException{
+    	BufferedReader reader = new BufferedReader(new FileReader(archivo));
+    	String linea;
+    	while ((linea = reader.readLine()) != null) {
+    		System.out.println(linea);
+    	}
+    	System.out.println();
+    }
+    
+    public void arte970836() throws IOException {
+    	imprimir("res/970836.txt");
+    }
+    
+    public static void main(String[] args) {
+    	AsciiArt arte = new AsciiArt();
+    	try {
+    		arte.arte970836();
+    	} catch (IOException e) {
+			System.out.println("Se dió el siguiente error: " + e.getMessage());
+		}
+    }
+
+}
