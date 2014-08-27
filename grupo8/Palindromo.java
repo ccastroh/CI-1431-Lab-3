993c181cb15d4f243c54d18142dde4026ab45789
analauber <lau3093@gmail.com>
2014-08-27 13:57:28 -0600

    analauber agregando grupo 8/palindromo.java

---
+++ grupo8/Palindromo.java
@@ -0,0 +1,40 @@
+import java.lang.StringBuilder;
+public class Palindromo
+{
+       
+    public static boolean esPalindromo(String texto)
+    {
+        boolean palindromo;
+        String [] textoArray;
+        StringBuilder temp = new StringBuilder();
+        texto = texto.trim().toLowerCase();
+        textoArray = texto.split("\\s+");
+        for(int i = 0; i < textoArray.length; i++)
+        {
+            temp.append(textoArray[i]);
+        }
+        String textoSinEspacios = temp.toString();
+        palindromo = textoSinEspacios.equals(new StringBuilder(textoSinEspacios).reverse().toString());
+        return palindromo;
+    } //hice este método porqué en las instrucciones decía que llamará a esPalindromo, no a palindromo
+
+    public static void pruebaPalindromo()
+    {
+        String pruebas [] = {"reconocer ", " Radar", "ABBA", "Carlos", "ABC"};
+
+        for(String p: pruebas)  
+        {
+            System.out.print("La hilera:" + p);
+            if(esPalindromo(p))
+            {
+                System.out.println(" SI es un palíndromo");
+            }else
+            {
+                System.out.println(" NO es un palíndromo");
+            }
+        }
+
+    }
+    
+    
+}
