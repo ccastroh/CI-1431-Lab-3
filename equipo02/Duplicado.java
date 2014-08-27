219de7d8ce1de2fbce8ca78f56359349cc12a6e9
Darg1492 <darg19920214@gmail.com>
2014-08-27 15:09:43 -0600

    cambios hechos

--- equipo02/Duplicado.java
+++ equipo02/Duplicado.java
@@ -1,47 +1,55 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Duplicado
-{
-  public static void llenarVector(int v[]) {
-    Random r = new Random();
-    for (int i = 0; i < v.length; i++) {
-        v[i] = r.nextInt(100);
-    }
-    }
-    public static boolean duplicados(int vector[]) {
-        byte b = 0;
-        for (int i = 0; i < vector.length;  i++) {
-            for (int j = 0; j < vector.length; j++) {
-        if (vector[i] == vector[j] && i != j) {
-            b = 1;
-            break;
-        }
-        }
-        if (b == 1) {
-            break;
-        }
-    }
-    if (b == 1) {
-        return true;
-} else {
-    return false;
-}
-}
-public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Digite la longitud deseada de su vector:");
-    int a = scan.nextInt();
-    int v[]  = new int[a];
-    Duplicado.llenarVector(v);
-    System.out.print("El vector: [ ");
-    for (int i : v) {
-        System.out.print(i + " ");
-    }
-    System.out.print("]");
-    if (Duplicado.duplicados(v)) {
-        System.out.println(" es duplicado");
-} else {
-    System.out.println(" no es duplicado");
-}
-}
+
+public class Duplicado{
+   public static void llenarVector(int v[]) {
+      Random r = new Random();
+      for (int i = 0; i < v.length; i++) {
+          v[i] = r.nextInt(100);
+      }
+   }
+    
+   public static boolean duplicados(int vector[]) {
+       byte b = 0;
+       int i = 0;
+       int j = 0;
+       boolean resultado = false;
+       while(i < vector.length){
+           while(j < vector.length) {
+               if (vector[i] == vector[j] && i != j) {
+                   b = 1;
+                   j = vector.length;
+               }
+	       j++;
+           }
+           if (b == 1) {
+              i = vector.length;
+           }
+       }
+
+       if (b == 1) {
+           resultado = true;
+       }
+
+       return resultado;
+   }
+
+   public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         System.out.println("Digite la longitud deseada de su vector:");
+         int a = scan.nextInt();
+         int v[]  = new int[a];
+         Duplicado.llenarVector(v);
+	 String fin = "El vector: [ ";
+         for (int i : v) {
+             fin +=i + " ";
+         }
+         fin += "]";
+         if (Duplicado.duplicados(v)) {
+             fin += " es duplicado";
+         }else {
+             fin += " no es duplicado";
+         }
+	 System.out.println(fin);
+   }    
 }
