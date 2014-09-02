d82a758395df858f0f4461fa16496c474e13330f
jennlv <jennylv17@gmail.com>
2014-09-02 00:14:47 -0600

    Corregido jennlv

--- equipo09/DuplicadosVector.java
+++ equipo09/DuplicadosVector.java
@@ -1,41 +1,51 @@
+/*
+ * Autor: Código aportado por el profesor Carlos Andres Castro
+ * Debería estar la licencia del Código   
+ */
+
 import java.util.Random;
 
+/**
+ * Clase que verifica si se encuentra algun elemento 
+ * duplicado en un vector 
+ */
 public class DuplicadosVector{
     
     private static int vectorNumeros[];
     
-    public DuplicadosVector(){
-        vectorNumeros = new int[3];
+    /** Construye un nuevo vector de enteros para ser implementado en la clase */
+    public DuplicadosVector(int dimension){
+        vectorNumeros = new int[dimension];
     }
     
-    public static void main (String args []) {
-        DuplicadosVector dv = new DuplicadosVector();
-        dv.llenarVector(vectorNumeros);
-        System.out.println(dv.duplicados(vectorNumeros));
-        
-    }
-    
-    
+    /** Método de tipo void llena el vector con números aleatorios del 1 al 99 */
     public static void llenarVector(int v[]) {
-        Random r = new Random();
+        Random random = new Random();
         for (int i = 0; i < v.length; i++) {
-            v[i] = r.nextInt(100);
+            v[i] = random.nextInt(100);
         }
-    }   
+    } 
+    
+    /** 
+     * Método que verifica si en el contenido del vector hay elementos repetidos
+     * Retorna el resultado de tipo booleano donde indica si hubieron elementos iguales o no 
+     */
     public static boolean duplicados(int vector[]){
-        boolean repetido = false;
-        
-        for (int i = 0; i < vector.length; i++){
+        boolean repetido = false;        
+        for (int i = 0; i < vector.length || repetido; i++){
             for (int j = i + 1; j < vector.length; j++){
                 if (vector[i] == vector[j]){
                     repetido = true;
-                    break;
                 }
-            }
-            if(repetido){
-                break;
             }
         }
         return repetido;
     }
+    
+    /** Main del programa, crear las intancias necesarias para la correcta ejecución*/
+    public static void main (String args []) {
+        DuplicadosVector duplicadoVector = new DuplicadosVector(5);
+        duplicadoVector.llenarVector(vectorNumeros);
+        System.out.println(duplicadoVector.duplicados(vectorNumeros));       
+    } 
 }
